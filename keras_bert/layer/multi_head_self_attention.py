9fb444b120aaa7b4478bf8a108e25b5171cf291c
Damian <damian.trybala@gmail.com>
2020-07-16 18:39:34 +0200

    Clean up of attention layer

--- keras_bert/layer/multi_head_self_attention.py
+++ keras_bert/layer/multi_head_self_attention.py
@@ -13,18 +13,25 @@ Multi Head Self Attention Layer class.
 class MultiHeadSelfAttention(Layer):
     def __init__(self, embedding_dim, num_heads):
         super(MultiHeadSelfAttention, self).__init__()
-        if embedding_dim % num_heads != 0:
-            raise ValueError(
-                f"embedding dimension = {embedding_dim} should be divisible by number of heads = {num_heads}"
-            )
+
         self.embedding_dim = embedding_dim
         self.num_heads = num_heads
         self.projection_dim = embedding_dim // num_heads
-        self.query_dense = Dense(embedding_dim)
-        self.key_dense = Dense(embedding_dim)
-        self.value_dense = Dense(embedding_dim)
+
+        self.check_correct_params()
+
+        self.query_layer = Dense(embedding_dim)
+        self.key_layer = Dense(embedding_dim)
+        self.value_layer = Dense(embedding_dim)
         self.combine_heads = Dense(embedding_dim)
 
+    def check_correct_params(self):
+        if self.embedding_dim % self.num_heads != 0:
+            raise ValueError(
+                "Wrong parameters, number of embedding dimensions should be divisable by number of heads"
+            )
+
+    """ Calculates attention for given vectors"""
     @staticmethod
     def attention(query, key, value):
         score = matmul(query, key, transpose_b=True)
@@ -32,36 +39,34 @@ class MultiHeadSelfAttention(Layer):
         scaled_score = score / math.sqrt(dim_key)
         weights = softmax(scaled_score, axis=-1)
         output = matmul(weights, value)
-        return output, weights
+        return output
 
+    """ Split word vectors into chunks of size num_heads, and divide work"""
     def separate_heads(self, x, batch_size):
         x = reshape(x, (batch_size, -1, self.num_heads, self.projection_dim))
         return transpose(x, perm=[0, 2, 1, 3])
 
+    """ Layer use self-attention, so we are using one input for all layers"""
     def __call__(self, inputs, mask):
-        # x.shape = [batch_size, seq_len, embedding_dim]
+
         batch_size = shape(inputs)[0]
-        query = self.query_dense(inputs)  # (batch_size, seq_len, embed_dim)
-        key = self.key_dense(inputs)  # (batch_size, seq_len, embed_dim)
-        value = self.value_dense(inputs)  # (batch_size, seq_len, embed_dim)
-        query = self.separate_heads(
-            query, batch_size
-        )  # (batch_size, num_heads, seq_len, projection_dim)
-        key = self.separate_heads(
-            key, batch_size
-        )  # (batch_size, num_heads, seq_len, projection_dim)
-        value = self.separate_heads(
-            value, batch_size
-        )  # (batch_size, num_heads, seq_len, projection_dim)
-        attention, weights = self.attention(query, key, value)
-        attention = transpose(
-            attention, perm=[0, 2, 1, 3]
-        )  # (batch_size, seq_len, num_heads, projection_dim)
-        concat_attention = reshape(
-            attention, (batch_size, -1, self.embedding_dim)
-        )  # (batch_size, seq_len, embed_dim)
-        output = self.combine_heads(
-            concat_attention
-        )  # (batch_size, seq_len, embed_dim)
+
+        """Pass input through all the base layers"""
+        query = self.query_layer(inputs)
+        key = self.key_layer(inputs)
+        value = self.value_layer(inputs)
+
+        """Splits vector representation of words into num_heads parts"""
+        query = self.separate_heads(query, batch_size)
+        key = self.separate_heads(key, batch_size)
+        value = self.separate_heads(value, batch_size)
+
+        """Calculate value of self-attention for parts"""
+        attention = self.attention(query, key, value)
+        attention = transpose(attention, perm=[0, 2, 1, 3])
+
+        """Combine information learnt by different heads"""
+        concat_attention = reshape(attention, (batch_size, -1, self.embedding_dim))
+        output = self.combine_heads(concat_attention)
         return output
 
